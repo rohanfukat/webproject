627e68f192d6db148736378613fc65acd1280330
rohanfukat <110030864+rohanfukat@users.noreply.github.com>
2023-03-19 19:46:56 +0530

    node js project first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 2000
+
+app.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`);
+})
+
+app.get("/",(req,resp)=>{
+    resp.json({"msg":"Hello heroku"})
+})
